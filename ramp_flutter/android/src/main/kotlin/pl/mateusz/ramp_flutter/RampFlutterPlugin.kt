359e0bb3ca519efc6f2066b51212e88c0c3bc901
Mateusz Jabłoński <mateusz.piotr.jablonski@gmail.com>
2021-09-22 11:53:31 +0200

    Converting purchase to map

--- ramp_flutter/android/src/main/kotlin/pl/mateusz/ramp_flutter/RampFlutterPlugin.kt
+++ ramp_flutter/android/src/main/kotlin/pl/mateusz/ramp_flutter/RampFlutterPlugin.kt
@@ -18,92 +18,115 @@ import network.ramp.sdk.facade.RampCallback
 import network.ramp.sdk.facade.RampSDK
 
 /** RampFlutterPlugin */
-class RampFlutterPlugin: FlutterPlugin, MethodCallHandler, ActivityAware {
-  private lateinit var channel : MethodChannel
-  private lateinit var context : Context
-  private lateinit var activity : Activity
-  private lateinit var rampSdk: RampSDK
+class RampFlutterPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
+    private lateinit var channel: MethodChannel
+    private lateinit var context: Context
+    private lateinit var activity: Activity
+    private lateinit var rampSdk: RampSDK
 
-  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "ramp_flutter")
-    channel.setMethodCallHandler(this)
-    context = flutterPluginBinding.applicationContext
-  }
-
-  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    if (call.method == "showRamp") {
-      showRamp(call.arguments)
-      result.success(null)
-    } else {
-      result.notImplemented()
+    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        channel = MethodChannel(flutterPluginBinding.binaryMessenger, "ramp_flutter")
+        channel.setMethodCallHandler(this)
+        context = flutterPluginBinding.applicationContext
     }
-  }
 
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
-  }
-
-  override fun onDetachedFromActivity() {
-    TODO("Not yet implemented")
-  }
-
-  override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
-    TODO("Not yet implemented")
-  }
-
-  override fun onAttachedToActivity(binding: ActivityPluginBinding) {
-    activity = binding.activity
-  }
-
-  override fun onDetachedFromActivityForConfigChanges() {
-    TODO("Not yet implemented")
-  }
-
-  private fun showRamp(arguments: Any) {
-    rampSdk = RampSDK()
-    val config = makeConfiguration(arguments) ?: return
-    val callback = object : RampCallback {
-      override fun onPurchaseFailed() {
-        channel.invokeMethod("onRampFailed", null)
-      }
-
-      override fun onPurchaseCreated(
-        purchase: Purchase,
-        purchaseViewToken: String,
-        apiUrl: String
-      ) {
-        val purchaseMap = serializePurchase(purchase)
-        channel.invokeMethod("onPurchaseCreated", purchaseMap)
-      }
-
-      override fun onWidgetClose() {
-        channel.invokeMethod("onRampClosed", null)
-      }
+    override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+        if (call.method == "showRamp") {
+            showRamp(call.arguments)
+            result.success(null)
+        } else {
+            result.notImplemented()
+        }
     }
-    rampSdk.startTransaction(activity, config, callback)
-  }
+
+    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+        channel.setMethodCallHandler(null)
+    }
+
+    override fun onDetachedFromActivity() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+        activity = binding.activity
+    }
+
+    override fun onDetachedFromActivityForConfigChanges() {
+        TODO("Not yet implemented")
+    }
+
+    private fun showRamp(arguments: Any) {
+        rampSdk = RampSDK()
+        val config = makeConfiguration(arguments) ?: return
+        val callback = object : RampCallback {
+            override fun onPurchaseFailed() {
+                channel.invokeMethod("onRampFailed", null)
+            }
+
+            override fun onPurchaseCreated(
+                purchase: Purchase,
+                purchaseViewToken: String,
+                apiUrl: String
+            ) {
+                val purchaseMap = serializePurchase(purchase)
+                channel.invokeMethod("onPurchaseCreated", purchaseMap)
+            }
+
+            override fun onWidgetClose() {
+                channel.invokeMethod("onRampClosed", null)
+            }
+        }
+        rampSdk.startTransaction(activity, config, callback)
+    }
 }
 
 private fun makeConfiguration(arguments: Any): Config? {
-  val map = arguments as? Map<*, *> ?: return null
-  val hostAppName = map["hostAppName"] as? String ?: "Ramp Integration"
-  val hostLogoUrl = map["hostLogoUrl"] as? String ?: "https://ramp.network/assets/images/Logo.svg"
-  val config = Config(hostAppName, hostLogoUrl)
-  
-  config.swapAsset = map["swapAsset"] as? String ?: ""
-  config.swapAmount = map["swapAmount"] as? String ?: ""
-  config.fiatCurrency = map["fiatCurrency"] as? String ?: ""
-  config.fiatValue = map["fiatValue"] as? String ?: ""
-  config.userAddress = map["userAddress"] as? String ?: ""
-  config.userEmailAddress = map["userEmailAddress"] as? String ?: ""
-  config.selectedCountryCode = map["selectedCountryCode"] as? String ?: ""
-  config.defaultAsset = map["defaultAsset"] as? String ?: ""
-  config.webhookStatusUrl = map["webhookStatusUrl"] as? String ?: ""
-  config.hostApiKey = map["hostApiKey"] as? String ?: ""
+    val map = arguments as? Map<*, *> ?: return null
+    val hostAppName = map["hostAppName"] as? String ?: "Ramp Integration"
+    val hostLogoUrl = map["hostLogoUrl"] as? String ?: "https://ramp.network/assets/images/Logo.svg"
+    val config = Config(hostAppName, hostLogoUrl)
 
-  return config
+    config.swapAsset = map["swapAsset"] as? String ?: ""
+    config.swapAmount = map["swapAmount"] as? String ?: ""
+    config.fiatCurrency = map["fiatCurrency"] as? String ?: ""
+    config.fiatValue = map["fiatValue"] as? String ?: ""
+    config.userAddress = map["userAddress"] as? String ?: ""
+    config.userEmailAddress = map["userEmailAddress"] as? String ?: ""
+    config.selectedCountryCode = map["selectedCountryCode"] as? String ?: ""
+    config.defaultAsset = map["defaultAsset"] as? String ?: ""
+    config.webhookStatusUrl = map["webhookStatusUrl"] as? String ?: ""
+    config.hostApiKey = map["hostApiKey"] as? String ?: ""
+
+    return config
 }
 
 private fun serializePurchase(purchase: Purchase): Map<String, Any?> {
-  return emptyMap()
+    return mapOf(
+        "id" to purchase.id,
+        "endTime" to purchase.endTime,
+        "asset" to mapOf(
+            "address" to purchase.asset.address,
+            "decimals" to purchase.asset.decimals,
+            "name" to purchase.asset.name,
+            "symbol" to purchase.asset.symbol,
+            "type" to purchase.asset.type,
+        ),
+        "receiverAddress" to purchase.receiverAddress,
+        "cryptoAmount" to purchase.cryptoAmount,
+        "fiatCurrency" to purchase.fiatCurrency,
+        "fiatValue" to purchase.fiatValue,
+        "assetExchangeRate" to purchase.assetExchangeRate,
+        "baseRampFee" to purchase.baseRampFee,
+        "networkFee" to purchase.networkFee,
+        "appliedFee" to purchase.appliedFee,
+        "paymentMethodType" to purchase.paymentMethodType,
+        "createdAt" to purchase.createdAt,
+        "updatedAt" to purchase.updatedAt,
+        "status" to purchase.status,
+        "escrowAddress" to purchase.escrowAddress,
+    )
 }
