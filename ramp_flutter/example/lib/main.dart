3cd5164a1dc7f7f6db0f2e98908cc10bde9b1c87
Mateusz Jabłoński <mateusz.piotr.jablonski@gmail.com>
2021-09-29 20:04:25 +0200

    Using platform widgets

--- ramp_flutter/example/lib/main.dart
+++ ramp_flutter/example/lib/main.dart
@@ -1,54 +1,113 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 
 import 'package:ramp_flutter/ramp_flutter.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const RampFlutterApp());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
+class RampFlutterApp extends StatefulWidget {
+  const RampFlutterApp({Key? key}) : super(key: key);
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  State<RampFlutterApp> createState() => _RampFlutterAppState();
 }
 
-class _MyAppState extends State<MyApp> {
+class _RampFlutterAppState extends State<RampFlutterApp> {
   final Configuration _configuration = Configuration();
+  final List<String> _environments = [
+    "https://ri-widget-dev.firebaseapp.com",
+    "https://ri-widget-staging.firebaseapp.com",
+    "https://buy.ramp.network",
+  ];
+
+  int _selectedEnvironment = 0;
+
+  @override
+  void initState() {
+    _configuration.fiatValue = "3";
+    _configuration.fiatCurrency = "EUR";
+    _configuration.defaultAsset = "ETH";
+    _configuration.userAddress = "0x4b7f8e04b82ad7f9e4b4cc9e1f81c5938e1b719f";
+    _configuration.hostAppName = "Ramp Flutter";
+    _configuration.deepLinkScheme = 'rampflutter';
+
+    super.initState();
+  }
+
+  void _selectEnvironment(int id) {
+    setState(() => _selectedEnvironment = id);
+  }
 
   @override
   Widget build(BuildContext context) {
-    _configuration.deepLinkScheme = 'rampflutterdemo';
+    _configuration.url = _environments[_selectedEnvironment];
 
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Ramp Demo in Flutter'),
-        ),
-        body: Builder(
-          builder: (context) => Form(
-            child: Column(children: _formFields(context)),
-          ),
-        ),
+    return PlatformApp(
+      home: PlatformScaffold(
+        appBar: PlatformAppBar(title: const Text('Ramp Flutter')),
+        body: Builder(builder: (c) => ListView(children: _formFields(c))),
+        iosContentPadding: false,
+        iosContentBottomPadding: false,
       ),
     );
   }
 
   List<Widget> _formFields(BuildContext context) {
     return [
-      TextField(
-        decoration: const InputDecoration(hintText: "Widget URL"),
-        onChanged: (text) => _configuration.url = text,
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          PlatformButton(
+            onPressed: () => _selectEnvironment(0),
+            child: const Text("dev"),
+          ),
+          PlatformButton(
+            onPressed: () => _selectEnvironment(1),
+            child: const Text("staging"),
+          ),
+          PlatformButton(
+            onPressed: () => _selectEnvironment(2),
+            child: const Text("production"),
+          ),
+        ],
       ),
-      TextField(
-        decoration: const InputDecoration(hintText: "User email"),
-        onChanged: (text) => _configuration.userEmailAddress = text,
+      PlatformText(
+        _environments[_selectedEnvironment],
+        style: const TextStyle(color: Color.fromRGBO(46, 190, 117, 1)),
       ),
-      TextField(
-        decoration: const InputDecoration(hintText: "User address"),
-        onChanged: (text) => _configuration.userAddress = text,
+      _textField(
+        "User email address",
+        (text) => _configuration.userEmailAddress = text,
+        _configuration.userEmailAddress,
       ),
-      TextButton(
+      _textField(
+        "Fiat value",
+        (text) => _configuration.fiatValue = text,
+        _configuration.fiatValue,
+      ),
+      _textField(
+        "Fiat currency",
+        (text) => _configuration.fiatCurrency = text,
+        _configuration.fiatCurrency,
+      ),
+      _textField(
+        "Default asset",
+        (text) => _configuration.defaultAsset = text,
+        _configuration.defaultAsset,
+      ),
+      _textField(
+        "User address",
+        (text) => _configuration.userAddress = text,
+        _configuration.userAddress,
+      ),
+      _textField(
+        "Host app name",
+        (text) => _configuration.hostAppName = text,
+        _configuration.hostAppName,
+      ),
+      PlatformButton(
         onPressed: () {
           RampFlutter.showRamp(
             _configuration,
@@ -60,6 +119,15 @@ class _MyAppState extends State<MyApp> {
         child: const Text("Show Ramp"),
       ),
     ];
+  }
+
+  PlatformTextField _textField(String placeholder,
+      void Function(String) onChanged, String? defaultValue) {
+    return PlatformTextField(
+      hintText: placeholder,
+      onChanged: onChanged,
+      controller: TextEditingController(text: defaultValue),
+    );
   }
 
   void _showSnackBar(BuildContext context, String text) {
