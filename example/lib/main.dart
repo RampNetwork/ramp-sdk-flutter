d9afc30704f04a2bf1ac6f4809fe5a64b5414b62
Mateusz Jabłoński <mateusz.piotr.jablonski@gmail.com>
2023-09-25 17:19:06 +0200

    Demo modal presentation

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -28,20 +28,51 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: const Text('RampWidget example')),
-        body: RampWidget(
-          onOnrampPurchaseCreated: (purchase, purchaseViewToken, apiUrl) =>
-              // ignore: avoid_print
-              print("Purchase created: $purchaseViewToken"),
-          onOfframpSaleCreated: (sale, saleViewToken, apiUrl) =>
-              // ignore: avoid_print
-              print("Sale created: $saleViewToken"),
-          onSendCryptoRequested: (payload) =>
-              // ignore: avoid_print
-              print("Send crypto requested: $payload"),
-          onRampClosed: () =>
-              // ignore: avoid_print
-              print("Ramp closed"),
-        ),
+        body: const BottomSheetExample(),
+      ),
+    );
+  }
+
+  Widget rampWidget() {
+    return RampWidget(
+      onOnrampPurchaseCreated: (purchase, token, apiUrl) =>
+          // ignore: avoid_print
+          print("Purchase created: $token"),
+      onOfframpSaleCreated: (sale, token, apiUrl) =>
+          // ignore: avoid_print
+          print("Sale created: $token"),
+      onSendCryptoRequested: (payload) =>
+          // ignore: avoid_print
+          print("Send crypto requested: $payload"),
+      onRampClosed: () =>
+          // ignore: avoid_print
+          print("Ramp closed"),
+    );
+  }
+}
+
+class BottomSheetExample extends StatelessWidget {
+  const BottomSheetExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+        child: const Text('showModalBottomSheet'),
+        onPressed: () {
+          showModalBottomSheet<void>(
+            context: context,
+            isScrollControlled: true,
+            enableDrag: false,
+            useSafeArea: true,
+            builder: (BuildContext context) {
+              return Container(
+                height: MediaQuery.of(context).size.height * 0.8,
+                child: const RampWidget(),
+              );
+            },
+          );
+        },
       ),
     );
   }
